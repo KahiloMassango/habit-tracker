62efbc304056c4b811099f15bde18841aa68e78a
KahiloMassango <kahilo42@gmail.com>
2025-05-22 10:40:03 +0100

    refactor: moving HabitDetailModal to home screen components

--- app/src/main/java/com/example/habittracker/features/home/component/HabitDetailModal.kt
+++ app/src/main/java/com/example/habittracker/features/home/component/HabitDetailModal.kt
@@ -1,4 +1,4 @@
-package com.example.habittracker.features.habitDetail
+package com.example.habittracker.features.home.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
