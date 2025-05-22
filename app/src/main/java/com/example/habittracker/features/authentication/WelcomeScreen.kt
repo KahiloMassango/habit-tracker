a2588d4f01dc7280054a45da15210b044e1a0d2d
KahiloMassango <kahilo42@gmail.com>
2025-05-22 08:33:38 +0100

    refactor: replace welcome img and image size

--- app/src/main/java/com/example/habittracker/features/authentication/WelcomeScreen.kt
+++ app/src/main/java/com/example/habittracker/features/authentication/WelcomeScreen.kt
@@ -18,12 +18,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.habittracker.R
 import com.example.habittracker.core.components.PrimaryButton
 import com.example.habittracker.core.theme.HabitTrackerTheme
